56845051878e0d8d12f1b24f170b084b8fd10ef9
Galina <koroleva2525.gk@gmail.com>
2022-12-05 13:53:28 +0100

    add Tasks 63 67 69

---
+++ Task63/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от 1 до N
+// Console.WriteLine("Enter N");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// for(int i = 1; i <= N; i++)
+// {
+    
+//     Console.Write(i);
+// }
+
+String Queue(int i, int n)
+{
+    if (i > n) return " ";
+    else return $"{i}," + Queue(i + 1, n);
+}
+Console.WriteLine(Queue(4,8));
---
+++ Task69/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа А и В, и возводит
+// число А в целую степень В с помощью рекурсии.
+//A = 3; В = 5 -> 243
+//A = 2; В = 3 -> 8
+int Proizv(int a, int b)
+{
+    int res = 1;
+    for(int i = 1; i <= b; i++) res = res*a;
+    return res;
+}
+
+int ProizvRek(int a, int b)
+{
+    if(b==0) return 1;
+    else return a * ProizvRek(a, b -1);
+
+}
+Console.WriteLine(Proizv(3,5));
+Console.WriteLine(ProizvRek(3,5));
